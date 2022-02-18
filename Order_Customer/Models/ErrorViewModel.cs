0a19dce6cba7cd454119a6cbcfe6f5530e559f01
roshan <roshanpandey@gmail.com>
2022-02-18 09:54:07 +0530

    chabged by roshan

---
+++ Order_Customer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Order_Customer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
